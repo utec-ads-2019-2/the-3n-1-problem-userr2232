f6812c655bd995397b42d19564332c6b8bc557a9
userr2232 <reynaldo.rz.26@gmail.com>
2019-08-21 20:00:51 -0500

    a

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <sstream>
+#include <algorithm>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int three_n_plus_one(int n, int cycle_length = 1) {
+    if(n == 1) return cycle_length;
+    if(n%2) n = 3*n+1;
+    else n /= 2;
+    return three_n_plus_one(n, ++cycle_length);
+}
+
+int main() {
+    int i, j;
+    string line;
+    while(getline(cin, line)) {
+        istringstream iss(line);
+        iss >> i >> j;
+        int max_cycle_length = 1;
+        for(int k = min(i,j); k <= max(i,j); ++k)
+            max_cycle_length = max(three_n_plus_one(k), max_cycle_length);
+        cout << i << " " << j << " " << max_cycle_length << endl;
+    }
     return 0;
 }
